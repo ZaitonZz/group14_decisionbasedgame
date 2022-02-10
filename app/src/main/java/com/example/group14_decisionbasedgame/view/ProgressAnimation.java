8b50c5fb4cd4b6de96b063d4305357bd9769af45
JayGamao <jonreff.gamao@yahoo.com>
2022-02-11 07:33:46 +0800

    Loading screen trial

---
+++ app/src/main/java/com/example/group14_decisionbasedgame/view/ProgressAnimation.java
@@ -0,0 +1,38 @@
+package com.example.group14_decisionbasedgame.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProgressAnimation extends Animation {
+    private Context context;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private float from;
+    private float to;
+
+
+    public ProgressAnimation(Context context, TextView textView, ProgressBar progressBar, float from, float to) {
+        this.context = context;
+        this.textView = textView;
+        this.progressBar = progressBar;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+        float value = from + (to - from) * interpolatedTime;
+
+        progressBar.setProgress((int) value);
+        textView.setText((int) value + "%");
+
+        if (value == to) {
+            context.startActivity(new Intent(context, default_starting_screen.class));
+        }
+    }
+}
--- app/src/main/java/com/example/group14_decisionbasedgame/view/loading_screen.java
+++ app/src/main/java/com/example/group14_decisionbasedgame/view/loading_screen.java
@@ -2,6 +2,7 @@ package com.example.group14_decisionbasedgame.view;
 
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,19 +11,31 @@ import com.example.group14_decisionbasedgame.R;
 
 public class loading_screen extends AppCompatActivity {
 
-    private int CurrentProgress = 0;
-    private ProgressBar progressBar;
+    ProgressBar progressBar;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_screen);
 
-        CurrentProgress = CurrentProgress + 10;
-        progressBar.setProgress(CurrentProgress);
+        progressBar = findViewById(R.id.pro);
+        textView = findViewById(R.id.txt);
+
         progressBar.setMax(100);
+        progressBar.setScaleY(2f);
+
+        progressAnimation();
+
     }
 
+    private void progressAnimation() {
+
+        ProgressAnimation animation =  new ProgressAnimation(this,textView,progressBar,0f,100f);
+        animation.setDuration(8000);
+        progressBar.setAnimation(animation);
+    }
 }
 
 
