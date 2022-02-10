55c42adb414dda1262c222b3aa9d036d58e8bcec
JayGamao <jonreff.gamao@yahoo.com>
2022-02-10 17:51:43 +0800

    Loading Screen Commit

--- app/src/main/java/com/example/group14_decisionbasedgame/view/loading_screen.java
+++ app/src/main/java/com/example/group14_decisionbasedgame/view/loading_screen.java
@@ -1,15 +1,29 @@
 package com.example.group14_decisionbasedgame.view;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.group14_decisionbasedgame.R;
 
+
 public class loading_screen extends AppCompatActivity {
+
+    private int CurrentProgress = 0;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_screen);
+
+        CurrentProgress = CurrentProgress + 10;
+        progressBar.setProgress(CurrentProgress);
+        progressBar.setMax(100);
     }
+
 }
+
+
+
