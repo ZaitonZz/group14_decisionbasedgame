f476f82e992a23bec0e6f188ee632951a8846089
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 16:59:15 +0800

    added a default starting screen (for when the btn_start is clicked) which will be created after the loading screen

---
+++ app/src/main/java/com/example/group14_decisionbasedgame/view/default_starting_screen.java
@@ -0,0 +1,15 @@
+package com.example.group14_decisionbasedgame.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.group14_decisionbasedgame.R;
+
+public class default_starting_screen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.default_starting_screen);
+    }
+}
