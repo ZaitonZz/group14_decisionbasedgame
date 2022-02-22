4bbc61472af9b98aaef1bba349ea61fbf3fbc5df
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 03:02:42 +0800

    commit 2 02/21/2022: added models where we would store the strings.

---
+++ app/src/main/java/com/example/group14_decisionbasedgame/model/appDialogue.java
@@ -0,0 +1,45 @@
+package com.example.group14_decisionbasedgame.model;
+
+import android.os.Bundle;
+
+public class appDialogue {
+    String D1_1 =" You're starting your last year as a Junior Highschool student at Malayan Colleges Mindanao and have to make an important decision!";
+    String D1_2 =" You have to pick a strand soon for senior highschool!";
+    String D1_3 ="A lot of decisions and factors to consider, the overwhelming thoughts are put away immediately as you start to venture in.";
+
+    String D2_1 ="You walk inside the building, but all of a sudden you bump into someone...";
+
+    String D3_1 ="(Bell Rings!) you’ve arrived at your classroom and it's time for introductions.";
+    String D3_2 ="After you and all your classmates have conversed you decide to go out and check out some of the clubs.";
+
+    String D4_1 ="You find yourself pacing through the halls, and take notice of a girl waiting out by the hall.";
+    String D4_2 = "You start thinking of what to say to or do, you pin it down to four possible outcomes,";
+    String Q1 = "[A. take your chances and join the club.] or [B. refuse the offer and keep looking for new clubs.] or [C. run away from the scene.] or [D. ignore the offer.]";
+
+    String D5_1 ="You enter the club room with the pretty senior, and see three girls glaring as you enter the room.";
+    String D5_2 ="You realise one of them was the girl you bumped into earlier this morning.";
+    String D5_3 ="You look at the girl you ran into this morning with pitiful eyes.";
+    String D5_4 ="A girl with… (description of humms senpais appearance) stands up and begins to speak with confidence.";
+    String D5_5 ="After her short introduction, HUMSS senpai is sits herself down, whilst a very shy individual takes the stand next.";
+    String D5_6 ="ICT senpai takes her seat. A few seconds go by with no one taking the stand,";
+    String D5_7 =" but you notice all of your seniors staring at the girl in (ABM senpais appearance).";
+    String D5_8 ="The senior who called you in for the club immediately interjects.";
+    String D5_9 ="You want to contribute and prove to your seniors that you are reliable,";
+    String D5_10="but the problem is you have no idea what the whole club is about.";
+    String D5_11="You scan the room for any clues that could give you an idea of what this is all for.";
+    String D5_12="Everyone in the room is staring and trying to come up with a name.";
+    String D5_13="You enter a sequence of events where the whole club needs a name. Thus you must decide what fate is waiting ahead!";
+    String Q2 = "[A. Station ] [B. Desmos ] [C. Kongo] [D. Give up on naming the group and then decide for themselves?]";
+    String D5_14="With the club meeting being adjourned, you all leave and head home.";
+
+    String D6_1 ="With the club meeting being adjourned, you all leave and head home.";
+    String D6_2 ="You enter the clubroom with your senpais and settle yourselves. You remember the topics you will be discussing today,";
+    String D6_3 ="and try to remember what device you chose to cover.";
+    String D6_4 ="It's now your faith to decide on what to choose again. What type of speech are you willing to do!?";
+    String Q3 = "[A. StreamDeck] [B. RGB to all devices] [C.Logitech C270 Desktop WebCam] [D. Desk Matt]";
+    String D6_5 ="HUMSS senpai continues to explain the Liquid Coolant device in more detail,";
+    String D6_6 ="while the rest listen with their undivided attention solely on her exquisite tone of voice that shapes the sentences everytime she speaks.";
+    String D6_7 ="You stand in front of all your peers and begin your presentation.";
+    String D6_8 ="MC-kun continues to casually speak about his/her device in a controlled tone.";
+
+}
--- app/src/main/java/com/example/group14_decisionbasedgame/model/model1.java
+++
@@ -1,5 +0,0 @@
-package com.example.group14_decisionbasedgame.model;
-
-public class model1 {
-    //TODO: rename model1 into dialogueRelated and initialize strings for scene 1,2,3.
-}
